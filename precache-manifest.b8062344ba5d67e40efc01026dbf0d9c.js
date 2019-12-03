012b1ce58d13d0bf9bcbe4ccb6f149b0c01a570c
Sam Kim <samk311@gmail.com>
2019-12-03 17:28:04 -0600

    Updates

--- precache-manifest.b8062344ba5d67e40efc01026dbf0d9c.js
+++ precache-manifest.b8062344ba5d67e40efc01026dbf0d9c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d760fe65c7b8b0df8d1a42fcd67e93bd",
+    "revision": "ac1a13ea73968bad82653597a86fc451",
     "url": "/idle-anno-redux/index.html"
   },
   {
-    "revision": "9d1675efdc3d39c82e37",
-    "url": "/idle-anno-redux/static/css/main.5a315fba.chunk.css"
+    "revision": "e6df87a7693296492fdc",
+    "url": "/idle-anno-redux/static/css/main.5817ed01.chunk.css"
   },
   {
     "revision": "26136a67110e639997c5",
     "url": "/idle-anno-redux/static/js/2.b81a1cb8.chunk.js"
   },
   {
-    "revision": "9d1675efdc3d39c82e37",
-    "url": "/idle-anno-redux/static/js/main.526c7e37.chunk.js"
+    "revision": "e6df87a7693296492fdc",
+    "url": "/idle-anno-redux/static/js/main.e0f054bf.chunk.js"
   },
   {
     "revision": "1478f9691442c9f526e9",
